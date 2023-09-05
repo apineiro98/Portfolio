0604987ec59869968e2f42b5de87be126e50f71c
apineiro98 <alejopineiro98@gmail.com>
2023-09-05 13:13:34 -0300

    Adding proyects and skills

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 ((d) => {
   const $skillBtn = d.querySelector('.toggle-btn-1'),
     $proyectBtn = d.querySelector('.toggle-btn-2'),
-    $proyectContent = d.querySelectorAll('.hide')
+    $proyectContent = d.querySelector('.proyects'),
+    $skillContent = d.querySelector('.skills')
   ;
 
+  let counter = 1;
+
   $proyectBtn.addEventListener('click', (e) => {
-    d.querySelector('.hide').classList.add('is-select');
-    d.querySelector('.hide').classList.remove('is-off');
+    if (counter == 1) {
+      $proyectBtn.classList.add('select');
+      $skillBtn.classList.remove('select');
+      $proyectContent.classList.remove('none');
+      $skillContent.classList.add('none');
+      counter = 0;
+    }
+  });
+
+  $skillBtn.addEventListener('click', (e) => {
+    if (counter == 0) {
+      $proyectContent.classList.add('none');
+      $skillContent.classList.remove('none');
+      $proyectBtn.classList.remove('select');
+      $skillBtn.classList.add('select');
+      counter = 1;
+    }
   });
 
 })(document);
