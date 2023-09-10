490829ef3607541a0c8a7662fdc620604eb558a0
apineiro98 <alejopineiro98@gmail.com>
2023-09-09 23:20:25 -0300

    c avanzo

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
 ((d) => {
-  const $skillBtn = d.querySelector('.toggle-btn-1'),
-    $proyectBtn = d.querySelector('.toggle-btn-2'),
-    $proyectContent = d.querySelector('.proyects'),
-    $skillContent = d.querySelector('.skills')
+  const $proyectBtn = d.querySelector('.toggle-btn-1'),
+    $skillBtn = d.querySelector('.toggle-btn-2')
   ;
 
-  let counter = 1;
-
-  $proyectContent.style.display = 'none';
-
   $proyectBtn.addEventListener('click', (e) => {
-    if (counter == 1) {
-      $proyectBtn.classList.add('select');
-      $skillBtn.classList.remove('select');
-      $proyectContent.style.display = 'grid';
-      $skillContent.style.display = 'none';
-      counter = 0;
-    }
+    $proyectBtn.classList.add('toggle-efect');
+    $skillBtn.classList.remove('toggle-efect');
   });
 
   $skillBtn.addEventListener('click', (e) => {
-    if (counter == 0) {
-      $proyectContent.style.display = 'none';
-      $skillContent.style.display = 'grid';
-      $proyectBtn.classList.remove('select');
-      $skillBtn.classList.add('select');
-      counter = 1;
-    }
+    $skillBtn.classList.add('toggle-efect');
+    $proyectBtn.classList.remove('toggle-efect');
   });
 
 })(document);
